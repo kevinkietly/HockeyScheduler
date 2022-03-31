87d7e5f9af5805c238e2a3dc671e4aa254a7fc8e
Kevin Ly <kevinly@alumni.ubc.ca>
2022-03-31 15:12:56 -0700

    Implemented insert panel

--- src/ui/MainWindow.java
+++ src/ui/MainWindow.java
@@ -3,11 +3,42 @@ package ui;
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
+    private JPanel mainPanel;
+    private JTabbedPane sidebarTabbedPane;
 
     public MainWindow() {
         super("Hockey Scheduler");
-        setResizable(false);
-        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        initializeMainPanel();
+        initializeSidebarTabbedPane();
+        mainPanel.add(sidebarTabbedPane);
+        this.setContentPane(mainPanel);
+        this.setVisible(true);
+        this.pack();
+        this.setResizable(false);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+    }
 
+    private void initializeMainPanel() {
+        mainPanel = new JPanel();
+    }
+
+    private void initializeSidebarTabbedPane() {
+        sidebarTabbedPane = new JTabbedPane();
+        sidebarTabbedPane.setTabPlacement(SwingConstants.LEFT);
+        initializeSidebarTabs();
+    }
+
+    private void initializeSidebarTabs() {
+        sidebarTabbedPane.addTab("Insert", new InsertPanel());
+        sidebarTabbedPane.addTab("Delete", null);
+        sidebarTabbedPane.addTab("Update", null);
+        sidebarTabbedPane.addTab("Selection", null);
+        sidebarTabbedPane.addTab("Projection", null);
+        sidebarTabbedPane.addTab("Join", null);
+        sidebarTabbedPane.addTab("Aggregation", null);
+        sidebarTabbedPane.addTab("Nested Aggregation with Group By", null);
+        sidebarTabbedPane.addTab("Division", null);
+        sidebarTabbedPane.addTab("View", null);
     }
 }
